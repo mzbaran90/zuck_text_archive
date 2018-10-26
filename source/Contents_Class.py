ad4fc0dfd45722e0faeef25d21ca57790d0f127c
Mason Edmison <mzbaran@uwm.edu>
2018-10-25 19:42:35 -0500

    added for loop to remove hash tags

--- source/Contents_Class.py
+++ source/Contents_Class.py
@@ -1,22 +1,19 @@
-import os
 import re
 
 
 
 class Contents():
 
-
     def __init__(self, file):
         self.file = file + '.txt'
 
 
-
-    def open_file_create_reader_object_instance(self, file):
-        with open(file, 'r', encoding='utf-8')as infile:
+    def open_file_create_reader_object_instance(self):
+        with open(self.file, 'r', encoding='utf-8')as infile:
             reader = infile.read()
             return reader
 
-    def get_contents_index(self):
+    def get_contents_index(self, reader):
 
         reader = self.open_file_create_reader_object_instance()
         content_pos = reader.find('##content##') + "##content##".__len__()
@@ -26,14 +23,26 @@ class Contents():
     def isolate_utterances(self,contents):
 
         regex = re.compile(r'^##.+?##', flags=re.DOTALL and re.MULTILINE)
-        names = regex.findall(contents)
+        names = regex.findall(contents) #list of names with hashtag symbols
+
+#iterate through list and replaces index with no hashtag name
+        for index, name in enumerate(names):
+            names[index] = re.sub('[##]*','', name)
+
         utterances = re.split(regex, contents)
+
         return names, utterances
 
+    def assign_key_and_value(self, names, utterances):
+        pass
+
     def content_creator(self):
-        self.reader = self.open_file_create_reader_object_instance(self.file)
-        self.contents = self.get_contents_index()
-        self.seperate_utterances = self.isolate_utterances(self.contents)
+        reader = self.open_file_create_reader_object_instance()
+        contents = self.get_contents_index(reader)
+        sep_names, sep_utterances = self.isolate_utterances(contents)
+        list_of_participant_values = self.assign_key_and_value(sep_names, sep_utterances)
+        self.isolate_utterances(contents)
+
 
 
 
