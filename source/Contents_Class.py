e62908d87ee39b06a9833d41df0fc2c09bf8456f
Mason Edmison <mzbaran@uwm.edu>
2018-11-05 10:49:27 -0600

    added exception handling to driver and contents_class

--- source/Contents_Class.py
+++ source/Contents_Class.py
@@ -15,8 +15,11 @@ class Contents():
 
     def get_contents_index(self, reader):
 
-        #reader = self.open_file_create_reader_object_instance()
         content_pos = reader.find('##content##') + "##content##".__len__()
+        print(content_pos)
+        if content_pos == 10:
+            print('check transcript - content not found for %s' % self.file)
+            return None
 
         return reader[content_pos:]
 
@@ -29,25 +32,39 @@ class Contents():
         for index, name in enumerate(names):
             names[index] = re.sub('[##]*','', name)
 
-        utterances = re.split(regex, contents)
+        utterances = [split for split in re.split(regex, contents) if not split.isspace()]
+
         return names, utterances
 
     def assign_key_and_value(self, names, utterances):
         list_of_participants = []
-        for i in range(len(names)):
-            participant = {}
-            participant[names[i]] = utterances[i+1].strip()
-            list_of_participants.append(participant)
-        return list_of_participants
+        if len(names) + 1 != len(utterances):
+            print('names and utterance entries are not syncing up...')
+
+        else:
+
+
+            for i in range(len(names)):
+                participant = {}
+                participant[names[i]] = utterances[i+1].strip()
+                list_of_participants.append(participant)
+            return list_of_participants
 
 
 
 
     def content_creator(self):
-        reader = self.open_file_create_reader_object_instance()
-        contents = self.get_contents_index(reader)
-        sep_names, sep_utterances = self.isolate_utterances(contents)
-        list_of_participants = self.assign_key_and_value(sep_names, sep_utterances)
+        try:
+            reader = self.open_file_create_reader_object_instance()
+            contents = self.get_contents_index(reader)
+            sep_names, sep_utterances = self.isolate_utterances(contents)
+            list_of_participants = self.assign_key_and_value(sep_names, sep_utterances)
+        except UnicodeDecodeError as e:
+            print('something wrong with' + self.file, e)
+            return None
+        except TypeError:
+            return None
+
 
         return list_of_participants
 
--- source/driver.py
+++ source/driver.py
@@ -21,14 +21,13 @@ def list_csv_metadata(single_file):
 
         reader = csv.DictReader(infile, fieldnames)
         for row in reader:
-            metadata = {}
 
             if row['record_id'] == single_file:
-
+                metadata = {}
                 for name in fieldnames:
                     metadata[name] = row[name]
-
                 return metadata
+
 
 def write_xml_file(single_file, xml_transcript):
     with open('/Users/MasonBaran/Desktop/zuck_txt_testing/xml_files/' + single_file +'.xml', 'w') as transcript_file:
@@ -37,21 +36,20 @@ def write_xml_file(single_file, xml_transcript):
 
         
 
-
 if __name__ == '__main__':
     set_directory()
     files_no_ext = [file.split('.')[0] for file in getfiles()]  # trims file extension so csv record_id can be parsed
 
     for single_file in files_no_ext:
 
-# creating instances of metadata and contents objects
-
-        content_instance = Contents_Class.Contents(single_file)
-        
-# calling methods withing objects to build metadata and contents dictionaries
+# calling methods within
         metadata = list_csv_metadata(single_file)
 
+        content_instance = Contents_Class.Contents(single_file)
         contents = content_instance.content_creator()
+        if contents == None:
+             print('problem with record:' + single_file)
+             break
 
 #   Creating instance of xml_transcript object
         xml_builder = XML_Builder.xml_builder(metadata, contents)
